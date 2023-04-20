42746d6762fd39c734497bb46bd8f323026d9c20
Diogo C. Pereira <130650392+vBital@users.noreply.github.com>
2023-04-20 16:08:06 +0100

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\diogo\\Desktop\\estagio\\flutter\\material-components-flutter-codelabs-101-starter\\material-components-flutter-codelabs-101-starter\\mdc_100_series" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\diogo\\Desktop\\estagio\\flutter\\material-components-flutter-codelabs-101-starter\\material-components-flutter-codelabs-101-starter\\mdc_100_series"
+  "FLUTTER_ROOT=C:\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\diogo\\Desktop\\estagio\\flutter\\material-components-flutter-codelabs-101-starter\\material-components-flutter-codelabs-101-starter\\mdc_100_series\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\diogo\\Desktop\\estagio\\flutter\\material-components-flutter-codelabs-101-starter\\material-components-flutter-codelabs-101-starter\\mdc_100_series"
+  "FLUTTER_TARGET=C:\\Users\\diogo\\Desktop\\estagio\\flutter\\material-components-flutter-codelabs-101-starter\\material-components-flutter-codelabs-101-starter\\mdc_100_series\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8zMGUxYzQzMDgyMTMyOWM0ZWJiMjUxYTM0ZTY1MmEzZjk3N2ExYzVlLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\diogo\\Desktop\\estagio\\flutter\\material-components-flutter-codelabs-101-starter\\material-components-flutter-codelabs-101-starter\\mdc_100_series\\.dart_tool\\package_config.json"
+)
